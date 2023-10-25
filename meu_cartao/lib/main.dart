3aa4031646bae9b98d0e5f325597044b5872837d
Mateus Demuno Baptista <mateus.baptista@telefonica.com>
2023-10-25 18:13:43 -0300

    update release

---
+++ meu_cartao/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Seu-Madruga-IA.webp'),
+              ),
+              Text("Herói Madruga",
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontFamily: "Pacifico",
+                      fontWeight: FontWeight.bold)),
+              Text(
+                "Programador",
+                style: TextStyle(
+                    fontFamily: 'SourceSans',
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(children: <Widget>[
+                  Icon(Icons.phone, color: Colors.teal),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "+55 11 98563 2451",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSans',
+                      fontSize: 20.0,
+                    ),
+                  )
+                ]),
+              )
+            ]),
+          ))));
+}
